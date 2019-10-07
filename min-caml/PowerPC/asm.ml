2b30a2e3e6b14619dfd9282fa93a5b1219a2ce7b
kengo hirata <s3beeeekbbn.t@gmail.com>
2019-10-07 18:15:42 +0900

    PowerPC を回復

--- min-caml/PowerPC/asm.ml
+++ min-caml/PowerPC/asm.ml
@@ -46,7 +46,7 @@ let fletd(x, e1, e2) = Let((x, Type.Float), e1, e2)
 let seq(e1, e2) = Let((Id.gentmp Type.Unit, Type.Unit), e1, e2)
 
 let regs = (* Array.init 27 (fun i -> Printf.sprintf "_R_%d" i) *)
-  [| "%r2" ; "%r5" ; "%r6" ; "%r7" ; "%r8" ; "%r9" ; "%r10";
+  [| "%r2"; "%r5"; "%r6"; "%r7"; "%r8"; "%r9"; "%r10";
      "%r11"; "%r12"; "%r13"; "%r14"; "%r15"; "%r16"; "%r17"; "%r18";
      "%r19"; "%r20"; "%r21"; "%r22"; "%r23"; "%r24"; "%r25"; "%r26";
      "%r27"; "%r28"; "%r29"; "%r30" |]
